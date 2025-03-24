0880e8efda52603b86397728b55ad9302d1dd93f
Pavel Yakovlev <brylouol@yandex.ru>
2025-03-24 10:39:03 +0400

    Изменена структура результатов в расчете

--- calculation.js
+++ calculation.js
@@ -3,49 +3,10 @@ const queries = require('./queries');
 function recoginzeAndCalculate(data) {
     console.log(data);
 
-    let sqOuterWalls =[]
-    let sqInnerWalls =[]
+    const results = [] // Результаты для каждого этажа отдельно
 
-    let woodForOuterHolders =[] // Количество досок на внешние стойки
-    let woodForSq =[] // Количество досок для основания
-    let woodForOpenings =[] // Количество досок на проемы
+    for (let i = 0; i < data.floors.length; i++) {
 
-    let woodForOuterWall = []
-    let woodThickness = []
-    let woodForOuterWallVolume = []
-
-    let sqOSB = []
-    let sqSteamWaterProofAndWindscreen = []
-    let sqInsulationOuterWalls = []
-    let insulationThickness = []
-    let insulationVolume = []
-
-    let woodForinnerWall = []
-    let woodForinnerWallOpenings = []
-    let woodForinnerWallSum = []
-    let innerWallThickness = []
-
-    let innerWallVolume = []
-    let sqOSBInnerWall = []
-
-
-
-    // Расчет для отдельных этажей
-
-    for(let i = 0; i < data.floors.length; i++) {
-
-// Общие данные
-        //
-        // data.floors.length - количество этажей по ТЗ. Как будто неправильно
-        //
-        sqOuterWalls[i] = data.floors[i].walls.perimeter * data.floors[i].walls.height * data.floors.length;
-        sqInnerWalls[i] = data.floors[i].walls.innerWallLength * data.floors[i].walls.height  * data.floors.length;
-        //
-        //
-        //
-// Внешние стены
-        woodForOuterHolders[i] = Math.ceil(data.floors[i].walls.perimeter/0.6 + 1);
-        woodForSq[i] = data.floors[i].walls.perimeter*2/3;
 
 // Расчет проемов
         let sqWindows=0;
@@ -56,7 +17,6 @@ function recoginzeAndCalculate(data) {
 
         let sqInternalDoors=0;
         let woodInternalDoors=0;
-//площадь окон, дверей и внутренних проемов
         for (let j = 0; j < data.floors[i].windows.length; j++) {
             sqWindows += data.floors[i].windows[j].width * data.floors[i].windows[j].height * data.floors[i].windows[j].count;
             woodWindows += (data.floors[i].windows[j]+data.floors[i].windows[j].height)*2*data.floors[i].windows[j].count;
@@ -69,40 +29,73 @@ function recoginzeAndCalculate(data) {
             sqInternalDoors += data.floors[i].internalDoors[j].width * data.floors[i].internalDoors[j].height * data.floors[i].internalDoors[j].count;
             woodInternalDoors += (data.floors[i].internalDoors[j]+data.floors[i].internalDoors[j].height)*2*data.floors[i].internalDoors[j].count;
         }
-        woodForOpenings[i] = Math.ceil((woodWindows + woodExternalDoors)/3);
 
-        woodForOuterWall[i] = woodForOuterHolders[i] + woodForSq[i] + woodForOpenings[i] // Итого досок на внешние стены
+        let numOfFloors;
+        if (i === 0){
+            numOfFloors = 2; // Пол и потолок для первого этажа
+        } else
+            numOfFloors = 1; // Только потолок для остальных этажей
+        const resultForFloor = {
+            // Общие данные
+            //
+            // data.floors.length - количество этажей по ТЗ. Как будто неправильно
+            //
+            baseData: {
+                sqOuterWalls: data.floors[i].perimeter * data.floors[i].height * data.floors.length,
+                sqInnerWalls: data.floors[i].innerWallLength * data.floors[i].height * data.floors.length,
+            },
+            //
+            //
+            //
+            // Внешние стены
+            outerWalls:{
+                woodForOuterHolders: Math.ceil(data.floors[i].perimeter/0.6 + 1),
+                woodForSq: data.floors[i].perimeter*2/3,
+                sqWindows: sqWindows,
+                sqExternalDoors: sqExternalDoors,
+                sqInternalDoors: sqInternalDoors,
+                woodWindows: woodWindows,
+                woodExternalDoors: woodExternalDoors,
+                woodInternalDoors: woodInternalDoors,
 
-        woodThickness[i] = data.floors[i].wallThickness; // Толщина досок на внешние стены
-        woodForOuterWallVolume[i] = woodForOuterWall[i] * 0.05 * 3 * woodThickness[i]; // Объем досок на внешние стены
+//площадь окон, дверей и внутренних проемов
+                woodForOpenings: Math.ceil((woodWindows + woodExternalDoors)/3),
+                woodForOuterWall: this.woodForOuterHolders + this.woodForSq + this.woodForOpenings, // Итого досок на внешние стены
+                woodThickness: data.floors[i].wallThickness, // Толщина досок на внешние стены
+                woodForOuterWallVolume: this.woodForOuterWall * 0.05 * 3 * this.woodThickness, // Объем досок на внешние стены
+                sqOSB: this.sqOuterWalls * 2 * 1.15, // Площадь OSB для внешних стен и внутренних стен
+                sqSteamWaterProofAndWindscreen: this.sqOuterWalls * 1.15, // Площадь парогидроизоляции и ветрозащиты для внешних стен
+                sqInsulationOuterWalls: this.sqOuterWalls * 1.1 - (sqWindows + sqExternalDoors), // Площадь утеплителя для внешних стен
+                insulationThickness: this.woodThickness, // Толщина утеплителя равна толщине досок
+                insulationVolume: this.sqInsulationOuterWalls * this.insulationThickness/1000, // Объем утеплителя
 
-        sqOSB[i] = sqOuterWalls[i] * 2 * 1.15; // Площадь OSB для внешних стен и внутренних стен
-
-        sqSteamWaterProofAndWindscreen[i] = sqOuterWalls[i] * 1.15; // Площадь парогидроизоляции и ветрозащиты для внешних стен
-
-        sqInsulationOuterWalls[i] = sqOuterWalls[i] * 1.1 - (sqWindows + sqExternalDoors); // Площадь утеплителя для внешних стен
-
-        insulationThickness[i] = woodThickness[i]; // Толщина утеплителя равна толщине досок
-
-        insulationVolume[i] = sqInsulationOuterWalls[i] * insulationThickness[i]/1000; // Объем утеплителя
+            },
 
 // Внутренние стены
+            innerWalls: {
+                woodForInnerWall: Math.ceil(data.floors[i].innerWallLength/0.6), // Количество досок на внутренние стены
+                woodForInnerWallOpenings: Math.ceil(woodInternalDoors/3), // Количество досок на внутренние проемы
+                woodForInnerWallSum: this.woodForInnerWall + this.woodForInnerWallOpenings, // Итого досок на внутренние стены
+                innerWallThickness: data.floors[i].innerWallThickness, // Толщина досок на внутренние стены
+                innerWallVolume: this.woodForInnerWallSum * this.innerWallThickness/1000 * 3 * 0.05, // Объем досок на внутренние стены
+                sqOSBInnerWall: this.sqInnerWalls * 2 * 1.15, // Площадь OSB для внутренних стен
 
-        woodForinnerWall[i] = Math.ceil(data.floors[i].innerWallLength/0.6); // Количество досок на внутренние стены
+            },
 
-        woodForinnerWallOpenings[i] = Math.ceil(woodInternalDoors/3); // Количество досок на внутренние проемы
+// Перекрытия
+            overlaps:{
+                wood: Math.ceil(data.floors[i].baseArea * 0.7),
+                woodThickness: data.floors[i].overlaps.floorThickness,
+                woodVolume: this.wood * this.woodThickness/1000 * 6 * 0.05, // Объем досок для перекрытия
+                sqOSB: data.floors[i].baseArea * 2 * 1.15 * numOfFloors,
+                sqSteamWaterProofAndWindscreen: data.floors[i].baseArea * 1.15,
+                sqInsulation: data.floors[i].baseArea * numOfFloors * 1.1,
+                insulationThickness: data.floors[i].overlaps.floorThickness,
+                insulationVolume: this.sqInsulation * this.insulationThickness/1000, // Объем утеплителя
+            }
+        }
 
-        woodForinnerWallSum[i] = woodForinnerWall[i] + woodForinnerWallOpenings[i]; // Итого досок на внутренние стены
-
-        innerWallThickness[i] = data.floors[i].innerWallThickness; // Толщина досок на внутренние стены
-
-        innerWallVolume[i] = woodForinnerWallSum[i] * innerWallThickness[i]/1000 * 3 * 0.05; // Объем досок на внутренние стены
-
-        sqOSBInnerWall[i] = sqInnerWalls[i] * 2 * 1.15; // Площадь OSB для внутренних стен
     }
-
-
-
 }
 
 module.exports = {
